c109f860d56c55ff0f728b8c5bf3853a9d397622
lhphong <phong2552001@gmail.com>
2024-01-03 17:45:07 +0700

    [UPDATE] caching voi caffeine cache, config cache

--- movie-api/src/main/java/dev/lephong/movies/Services/ReviewServiceImpl.java
+++ movie-api/src/main/java/dev/lephong/movies/Services/ReviewServiceImpl.java
@@ -3,6 +3,7 @@ package dev.lephong.movies.Services;
 import dev.lephong.movies.Models.Movie;
 import dev.lephong.movies.Models.Review;
 import dev.lephong.movies.Repositories.ReviewRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CachePut;
@@ -10,7 +11,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
-
+@Slf4j
 @Service
 @CacheConfig(cacheNames = "review")
 public class ReviewServiceImpl implements ReviewService{
@@ -22,10 +23,14 @@ public class ReviewServiceImpl implements ReviewService{
     @Override
     public Review createReview(String reviewBody, String imdbId) {
         Review review = reviewRepository.insert(new Review(reviewBody));
+        log.info("Insert new review: {}" + review);
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
                 .apply(new Update().push("reviewIds").value(review))
                 .first();
+        if(log.isDebugEnabled()){
+            log.debug("Update movie.");
+        }
         return review;
     }
 }
